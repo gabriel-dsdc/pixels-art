1035b7a169fa52005a81432f46452f820677889a
gabriel-dsdc <gabriel.ds.duarte99@gmail.com>
2022-03-22 07:37:08 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const colors = ['red', 'blue', 'green'];
 const paletteSelector = document.getElementsByClassName('color');
+const boardSelector = document.getElementById('pixel-board');
 
 function setPaletteColors() {
   const paletteKeys = Object.keys(paletteSelector);
@@ -10,3 +11,22 @@ function setPaletteColors() {
   });
 }
 setPaletteColors();
+
+function createPixel(pixelClass) {
+  const pixel = document.createElement('div');
+  if (pixelClass !== undefined) {
+    pixel.classList = pixelClass;
+  }
+  return pixel;
+}
+
+function generatePixelBoard() {
+  for (let count = 0; count < 5; count += 1) {
+    boardSelector.appendChild(createPixel());
+    for (let count2 = 0; count2 < 5; count2 += 1) {
+      const mainDiv = document.querySelectorAll('#pixel-board > div')[count];
+      mainDiv.appendChild(createPixel('pixel'));
+    }
+  }
+}
+generatePixelBoard();
