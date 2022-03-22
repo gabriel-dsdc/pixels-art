54e4a2094604aaaed470221bdda1263feee6b3c1
gabriel-dsdc <gabriel.ds.duarte99@gmail.com>
2022-03-22 04:09:49 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,13 @@
+const colors = ['red', 'blue', 'green'];
+const paletteSelector = document.getElementsByClassName('color');
+
+function setPaletteColors() {
+  const paletteKeys = Object.keys(paletteSelector);
+  paletteKeys.forEach((key) => {
+    if (paletteKeys[key] !== '0') {
+      paletteSelector[key].style.backgroundColor = colors[key - 1];
+      console.log(key);
+    }
+  });
+}
+setPaletteColors();
